d96da596a0dea12244c6b98e4a3e3bb2d52ccd58
Elizabeth Yeh <esnider1699@gmail.com>
2018-12-06 22:18:56 -0700

    Updated index.js, Player class, and initiated TDD

---
+++ test/Player-test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Player = require('../Player.js');
+const spies = require('chai-spies');
+chai.use(spies);
+
+global.domUpdates = require('../domUpdates.js');
+chai.spy.on(global.domUpdates, ['changeScoreboard'], () => true);
+
+describe ('Player', function() {
+  var player;
+  beforeEach(function() {
+  player = new Player(player1);
+});
+
+it('should return true', function() {
+  expect(true).to.equal(true);
+});
+
+it('should increase the players score', function() {
+
+  player.increaseScore(100);
+  expect(player.increaseScore).to.equal(100);
+});
+
+it('should decrease the players score', function() {
+
+  player.decreaseScore(100);
+  expect(player.decreaseScore).to.equal(100);
+});
